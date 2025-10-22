59651220604c51c81f2e7c923fbc1745c9194312
Alok Bhadauria <142592948+alok-bhadauria@users.noreply.github.com>
2025-10-22 12:21:36 +0530

    Add files via upload

---
+++ fitness_app/db.js
@@ -0,0 +1,17 @@
+// utils/db.js
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  user: process.env.DB_USER || "fitness_user",
+  host: process.env.DB_HOST || "localhost",
+  database: process.env.DB_NAME || "fitnesscoach",
+  password: process.env.DB_PASSWORD || "AdminPass123",
+  port: process.env.DB_PORT || 5432,
+});
+
+pool.connect()
+  .then(() => console.log("✅ Connected to PostgreSQL database"))
+  .catch(err => console.error("❌ Database connection error:", err.stack));
+
+module.exports = pool;
