46f4868dc3bf246559e766a4a8f8cd7210690dda
Alok Bhadauria <bhadauriaalok123@gmail.com>
2025-11-29 10:03:02 +0530

    update

--- ml/app.py
+++ ml/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 import joblib
 import numpy as np
 import json
@@ -74,4 +75,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
